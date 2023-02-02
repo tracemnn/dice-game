e687a4352052d43fa085cbf79c08a76c5c26c51d
Buya <tracemnn2@gmail.com>
2023-02-02 23:52:15 +0800

    done

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         scores[activePlayer] = scores[activePlayer] + currentScore;
         document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
     
-        if(scores[activePlayer] >= 10 ) {
+        if(scores[activePlayer] >= 100 ) {
             isNewGame = false;
             document.getElementById('name-' + activePlayer).textContent = 'WINNER !!!';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add("winner");
